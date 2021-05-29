6ddbc70e6dc022c943f2c573fbfb33852186d1ec
Michele Rosso <mrosso@lbl.gov>
2021-05-28 23:58:20 -0700

    CMake: upgrade

--- configuration/chroma-config.cmake
+++ configuration/chroma-config.cmake
@@ -1,12 +1,80 @@
-# Take relevant flags from chroma configuration (call chroma-config). If we can't find chroma, we complain.
+#[=======================================================================[:
+FindCHROMA
+-------
 
-find_program(CHROMA_CONFIG NAMES chroma-config)
+Finds the CHROMA library.
 
-if( NOT CHROMA_CONFIG ) 
-  message(FATAL_ERROR "Dude, I can't find where your chroma is. I am sorry, I have to exit now. :(")  
-endif( NOT CHROMA_CONFIG)
+Imported Targets
+^^^^^^^^^^^^^^^^
 
-execute_process(COMMAND ${CHROMA_CONFIG} --cxxflags OUTPUT_VARIABLE CHROMA_CXX_FLAGS  OUTPUT_STRIP_TRAILING_WHITESPACE)
-execute_process(COMMAND ${CHROMA_CONFIG} --ldflags OUTPUT_VARIABLE CHROMA_LD_FLAGS OUTPUT_STRIP_TRAILING_WHITESPACE)
-execute_process(COMMAND ${CHROMA_CONFIG} --libs  OUTPUT_VARIABLE CHROMA_LIBS OUTPUT_STRIP_TRAILING_WHITESPACE)
+This module provides the following imported target, if found:
 
+``CHROMA``
+  The CHROMA library
+
+Result Variables
+^^^^^^^^^^^^^^^^
+
+This will define the following variables:
+
+``CHROMA_FOUND``
+  True if the CHROMA library has been found.
+``CHROMA_CXX_FLAGS``
+  The CXX flags of the CHROMA library which was found.
+``CHROMA_INCLUDE_DIRS``
+  Include directories needed to use CHROMA.
+``CHROMA_LD_FLAGS``
+  Linker flags needed to link to CHROMA.
+``CHROMA_LIBRARIES``
+  Libraries needed to link to CHROMA.
+#]=======================================================================]
+
+find_program( _chroma_config_exe
+   NAMES chroma-config
+   PATHS ${CHROMA_ROOT}/bin ENV CHROMA_ROOT
+   )
+
+if ( _chroma_config_exe )
+   set(CHROMA_INCLUDE_DIRS ${CHROMA_ROOT}/include)
+   execute_process(COMMAND ${_chroma_config_exe} --version
+      OUTPUT_VARIABLE CHROMA_VERSION
+      OUTPUT_STRIP_TRAILING_WHITESPACE)
+   execute_process(COMMAND ${_chroma_config_exe} --cxxflags
+      OUTPUT_VARIABLE CHROMA_CXX_FLAGS
+      OUTPUT_STRIP_TRAILING_WHITESPACE)
+   execute_process(COMMAND ${_chroma_config_exe} --ldflags
+      OUTPUT_VARIABLE CHROMA_LD_FLAGS
+      OUTPUT_STRIP_TRAILING_WHITESPACE)
+   execute_process(COMMAND ${_chroma_config_exe} --libs
+      OUTPUT_VARIABLE CHROMA_LIBRARIES
+      OUTPUT_STRIP_TRAILING_WHITESPACE)
+endif ()
+
+include(FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args(CHROMA
+   REQUIRED_VARS
+   CHROMA_LIBRARIES
+   CHROMA_INCLUDE_DIRS
+   CHROMA_LD_FLAGS
+   CHROMA_CXX_FLAGS
+   VERSION_VAR
+   CHROMA_VERSION
+   )
+
+mark_as_advanced(CHROMA_VERSION CHROMA_LIBRARIES CHROMA_CXX_FLAGS
+   CHROMA_INCLUDE_DIRS CHROMA_LD_FLAGS)
+
+# Create imported target
+if (CHROMA_FOUND AND NOT TARGET CHROMA)
+   # add_library(CHROMA UNKNOWN IMPORTED GLOBAL)
+   add_library(CHROMA INTERFACE)
+   set_target_properties(CHROMA
+      PROPERTIES
+      #IMPORTED_LOCATION "${CHROMA_LIBRARIES}"
+      INTERFACE_COMPILE_OPTIONS "${CHROMA_CXX_FLAGS}"
+      INTERFACE_INCLUDE_DIRECTORIES "${CHROMA_INCLUDE_DIRS}"
+      INTERFACE_LINK_OPTIONS "${CHROMA_LD_FLAGS}"
+      INTERFACE_LINK_LIBRARIES "${CHROMA_LIBRARIES}"
+      )
+endif ()
