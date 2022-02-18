120d681b693d0d75179a23f7258705fb32550758
Andre Walker-Loud <awalker-loud@lbl.gov>
2022-02-18 15:25:45 -0800

    a fix for the DR to DP spin rotation that occurs with QDPjit

--- lib/util/spin_basis.cc
+++ lib/util/spin_basis.cc
@@ -13,18 +13,23 @@
 namespace Chroma 
 { 
 
-  void rotate_to_Dirac_Basis(LatticePropagator & quark_to_be_rotated)
-  {
-    //I am lazy so I copy Robert to rotate this stuff...
-    SpinMatrix U = DiracToDRMat();
-    quark_to_be_rotated = adj(U)*quark_to_be_rotated*U ;
+    void rotate_to_Dirac_Basis(LatticePropagator & quark_to_be_rotated)
+    {
+        //I am lazy so I copy Robert to rotate this stuff...
+        SpinMatrix U = DiracToDRMat();
+        // QDPjit does not like a direct manipulation of the prop, so we need to make a temporary copy
+        LatticePropagator q_tmp;
+        q_tmp               = adj(U)*quark_to_be_rotated*U ;
+        quark_to_be_rotated = q_tmp;
+    }
 
-  }
-
-  void rotate_from_Dirac_Basis(LatticePropagator & quark_to_be_rotated)
-  {
-    SpinMatrix U = DiracToDRMat();
-    quark_to_be_rotated = U*quark_to_be_rotated*adj(U);
-  }
+    void rotate_from_Dirac_Basis(LatticePropagator & quark_to_be_rotated)
+    {
+        SpinMatrix U = DiracToDRMat();
+        // QDPjit does not like a direct manipulation of the prop, so we need to make a temporary copy
+        LatticePropagator q_tmp;
+        q_tmp               = U*quark_to_be_rotated*adj(U);
+        quark_to_be_rotated = q_tmp;
+    }
 
 } // End namespace Chroma
